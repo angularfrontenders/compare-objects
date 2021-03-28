f4e69c85cc4f009aadfe187c9b50b38c12fe527d
Angular Frontenders <admin@angularfrontenders.com>
2021-03-28 13:08:02 +0200

    Compare objects

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -16,16 +16,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'compare-objects'`, () => {
+  it(`should have as title 'Compare objects'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('compare-objects');
+    expect(app.title).toEqual('Compare objects');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('compare-objects app is running!');
+    expect(compiled.querySelector('h1').textContent).toContain('Compare objects');
   });
 });
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,43 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { IModel } from './model/iModel';
+import { ComparerService } from './comparer/comparer.service';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'compare-objects';
+export class AppComponent implements OnInit {
+
+  title = 'Compare objects';
+  objectA: IModel;
+  objectB: IModel;
+  objectC: IModel;
+  resultEqualsAB: boolean;
+  resultEqualsAC: boolean;
+
+  constructor(private _comparerService: ComparerService) {
+
+  }
+
+  ngOnInit(): void {
+    this.objectA = {
+      id: 1,
+      name: 'test46',
+      age: 46
+    };
+    this.objectB = {
+      id: 2,
+      name: 'test18',
+      age: 18
+    };
+    this.objectC = {
+      id: 1,
+      name: 'test46',
+      age: 46
+    };
+
+    // comparing
+    this.resultEqualsAB = this._comparerService.Equals(this.objectA, this.objectB);
+    this.resultEqualsAC = this._comparerService.Equals(this.objectA, this.objectC);
+  }
 }
